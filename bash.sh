2babcfb9138c64c68d61bf015eaff6a8ea0e141f
Artem Tsybakov <tsybakov.aa@mail.ru>
2024-07-17 15:37:19 +0300

    test

--- bash.sh
+++ bash.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-mkdir git
-cd git
-git clone https://github.com/artemtsybakov/shvirtd-example-python.git && cd git/shvirtd-example-python && docker compose up -d
+cd /opt/
+git clone https://github.com/artemtsybakov/shvirtd-example-python.git
+cd shvirtd-example-python
+docker compose up -d
